73fad51bc9130e7989f7d16175bb1de1d95dda61
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-12-13 18:10:40 -0500

    completed lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,27 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true if the word is a palindrome, returns false is word is not a palindrome", () => {
+    const palindrome = "racecar";
+
+    const response1 = isPalindrome(palindrome);
+
+    expect(response1).toBe(true);
+    
+    const notPalindrome="angel"
+
+    const response2 = isPalindrome(notPalindrome)
+
+    expect(response2).toBe(false)
+  });
+});
+
+
+it("handles uppercase and lowercase input", () => {
+  const  mixedCasePalindrome = "RAceCar";
+
+  const mixedCasePalindromeResponce = isPalindrome(mixedCasePalindrome);
+
+  expect(mixedCasePalindromeResponce).toBe(true);
+});
